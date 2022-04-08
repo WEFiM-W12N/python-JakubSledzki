7d8ab14c911026ce825573ea0a66f76273a19688
JakubSledzki <kubasledzki123@gmail.com>
2022-04-08 13:22:17 +0200

    zadanie 1 i 2
    stworzenie strony internetowej przy pomocy django, kod z użyciem klas oraz obiektów

---
+++ main.py
@@ -0,0 +1,20 @@
+from dis import dis
+
+
+class Student:
+    def check(self):
+        if self.marks >= 40:
+            return True
+        else:
+            return False
+
+    def __init__(self,name,marks):
+        self.name = name
+        self.marks = marks
+
+student1 = Student("daweed", 85)
+student2 = Student("kociiol",30)
+did_pass = student1.check()
+print(did_pass)
+did_pass = student2.check()
+print(did_pass)                     
