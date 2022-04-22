0300e08068fd3502b7bad478f0fd34fb5f184cf9
JakubSledzki <kubasledzki123@gmail.com>
2022-04-22 13:20:08 +0200

    ćwaiczenie 3
    lista pytań
    odpowiedzi w pytaniu pierwszym sql?
    łącza z listy do pytania

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,27 +8,10 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
-
-def index(request):
-    return HttpResponse("Pytania.")
-# Create your views here.
-
     
 def detail(request, question_id):
-    return HttpResponse("Pytanie nr. %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
     
 def results(request, question_id):
     response = "Wyniki pytania nr. %s."
